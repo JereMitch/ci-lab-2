6819c1b6f94d5c5853b97c5b4ceb9b6b20da021e
JereMitch <jere.mitch8@gmail.com>
2021-09-27 15:09:03 -0600

    re added my function

--- project.js
+++ project.js
@@ -2,9 +2,9 @@ function getGreeting(name){
     return `Hi ${name}!`;
 }
 
-// function getColor(color){
-//     return `This is ${color}!`  
-// }
+function getColor(color){
+    return `This is ${color}!`  
+}
 
 module.exports = getGreeting;
-// module.exports = getColor;
+module.exports = getColor;
