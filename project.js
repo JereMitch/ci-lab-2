a3923da30f4552ae8695b55be08dc481a8d9daee
JereMitch <jere.mitch8@gmail.com>
2021-09-27 15:02:26 -0600

    added a test

--- project.js
+++ project.js
@@ -2,4 +2,9 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
+function getColor(color){
+    return `This is ${color}!`  
+}
+
 module.exports = getGreeting;
+module.exports = getColor;
